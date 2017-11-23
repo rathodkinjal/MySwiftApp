ad653f622cda12b287a6add888382b860d28408f
Kinjal Rathod <Kinjal_Rathod@intuit.com>
2017-11-23 15:16:22 -0800

    Adding Calculations

--- MySwiftApp/ViewController.swift
+++ MySwiftApp/ViewController.swift
@@ -11,17 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
-    var counter = 0
+    //var counter = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
         myLabel.text = "Hello Kinjal Rathod!"
-        print("Button tapped")
+        /*print("Button tapped")
         counter = counter + 1
         
         if(counter > 10){
             myLabel.text = "You have tapped the button more than 10 times!"
-        }
+        }*/
         
+        print(text1.text!)
+        print(text2.text!)
+        
+        var sum = "Sum is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        myLabel.text = sum
     }
     override func viewDidLoad() {
         super.viewDidLoad()
